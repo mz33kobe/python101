88017f27f66138c21ca85062422d99fd1d01f7c8
mz33kobe <ndekerekiritu@gmail.com>
2020-09-23 06:21:13 +0300

    random_commit!

---
+++ fun_ctions.py
@@ -0,0 +1,16 @@
+print("xoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxxoxoxoxoxoxo")
+print("o===============KENYAN MUSIC OOH!!==================x")
+print("x=============Magoe by Zzero Sufuri=================o")
+print("o=====================Enjoy!!=======================x")
+print("xoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxox")
+
+def sample_function() :
+    print("Mi siezi kaa,")
+    print("Kejja iko na magoe")
+    print("Kejja iko na magoe")
+
+sample_function()
+sample_function()
+sample_function()
+sample_function()
+sample_function()
