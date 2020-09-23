6947991f05dc7186af012cb46214b7693fba1cfe
mz33kobe <ndekerekiritu@gmail.com>
2020-09-23 05:17:56 +0300

    RANDOM COMMIT

--- ex_3_3.py
+++ ex_3_3.py
@@ -1,8 +1,16 @@
 scoo = input("Enter a score between 0.0 and 1.0: ")
-scooo = float(scoo)
+grade = float(scoo)
 
-if (scooo >=0.0 & <= 1.0 ) :
-    
-
+if (grade >=0.0 and grade <= 1.0 ) :  
+    if (grade >= 0.9):
+        print("A")
+elif(grade >= 0.8):
+    print("B")
+elif(grade >= 0.7):
+    print("C")
+elif(grade >= 0.6):
+    print("D")
+elif(grade < 0.6):
+    print("F")            
 else:
     print("Please follow the instructions!")
