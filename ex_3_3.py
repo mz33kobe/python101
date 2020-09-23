4bf3f016a6df186b198e49b787d10ef2219d51f8
mz33kobe <ndekerekiritu@gmail.com>
2020-09-23 05:34:56 +0300

    random commit

--- ex_3_3.py
+++ ex_3_3.py
@@ -1,9 +1,10 @@
 scoo = input("Enter a score between 0.0 and 1.0: ")
 grade = float(scoo)
 
-if (grade >=0.0 and grade <= 1.0 ) :  
-    if (grade >= 0.9):
-        print("A")
+if 0.0 < grade > 1.0:
+    print("Please follow the instructions!")
+elif(grade >= 0.9):
+    print("A")    
 elif(grade >= 0.8):
     print("B")
 elif(grade >= 0.7):
@@ -12,5 +13,16 @@ elif(grade >= 0.6):
     print("D")
 elif(grade < 0.6):
     print("F")            
-else:
-    print("Please follow the instructions!")
+
+#Write a program to prompt for a score between 0.0 and 1.0. 
+# If the score is out of range, print an error. 
+# If the score is between 0.0 and 1.0, 
+# print a grade using the following table:
+# Score Grade
+#>= 0.9 A
+#>= 0.8 B
+#>= 0.7 C
+#>= 0.6 D
+#< 0.6 F
+#If the user enters a value out of range, print a suitable error message and exit.
+#  For the test, enter a score of 0.85.
