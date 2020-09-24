235665414f4e98ff907883b047cec0e5d4412356
mz33kobe <ndekerekiritu@gmail.com>
2020-09-24 09:26:39 +0300

    Parameters are hard

--- params.py
+++ params.py
@@ -1,8 +1,13 @@
+# Parameters in Python
+
 def greet(lang):
-    if lang = 'es':
+    if lang == 'es':
         print("Hola")
-    elif lang = 'fr':
+    elif lang == 'fr':
         print('Bonjour')
-     else:
-         print("Hello")       
+    else:
+        print("Hello")       
 greet('fr')
+greet('en')
+greet('en')
+greet('en')
